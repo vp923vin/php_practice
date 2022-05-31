dab0a6c5313182340a6fe57aa0d0d59cc0e50784
Vipin Patel <vp923vin@gmail.com>
2022-05-31 12:09:21 +0530

    php destructor

---
+++ php03.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oops concepts</title>
+</head>
+<body>
+    <?php
+       //properties
+       class student{
+           public $name;
+           public $stands;
+           public $DOB;
+           public $marks;
+           public $grade;
+           
+           // methods
+           function __construct($name, $stands, $DOB, $marks, $grade){
+               $this->name = $name;
+               $this->stands = $stands;
+               $this->DOB = $DOB;
+               $this->marks = $marks;
+               $this->grade = $grade;
+
+           }
+            // this function runs on script ending and here this fuction runs for decending order as object created
+           function __destruct(){
+               echo "Student {$this->name} class {$this->stands} 
+               date-of-birth {$this->DOB} got {$this->marks} precentage and his grade is {$this->grade}.";
+               echo "<br>";   
+           }
+       }
+        // object created here
+       $Ajay = new student("Ajay", "11th", "15-10-1997", "80%", "A" );
+       $vipin = new student("vipin", "12th", "5-10-1997", "85%", "A" );
+       // messages 
+       echo "student marks info <br>";
+    ?>
+    
+</body>
+</html>
