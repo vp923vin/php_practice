2268461a337224135914695f2cbe196da39ec414
Vipin Patel <vp923vin@gmail.com>
2022-06-01 12:47:18 +0530

    php Abstract class and Methods

--- php010.php
+++ php010.php
@@ -8,8 +8,53 @@
 </head>
 <body>
    <?php
-       
+       abstract class car{
+           public $name;
+           public $color;
+           public function __construct($name, $color){
+               $this->name = $name;
+               $this->color = $color;
+           }
+           abstract function Message() ;
+       } 
+
+       class Tata extends car{
+           function Message(){
+             echo "$this->name car is amazing  $this->color looks great in this car <br>"; 
+           }
+       }
+
+       class Mahindra extends car{
+        function Message(){
+          echo "$this->name car is shines in the dessert and  $this->color color is killer. <br>"; 
+        }
+    }
+
+    class Maruti extends car{
+        function Message(){
+          echo "$this->name car is amazing  $this->color looks great in this car. <br>"; 
+        }
+    }
+
+    class Hyundai extends car{
+        function Message(){
+          echo "$this->name car is fabulous  $this->color looks great in this car. <br>"; 
+        }
+    }
+
+       $Tata = new Tata("Tata","black");
+       $Tata->Message();
+
+       $Maruti = new Maruti("Maruti Suzuki","white");
+       $Maruti->Message();
+
+       $Mahindra = new Mahindra("Mahindra thar","Red-black");
+       $Mahindra->Message();
+
+       $Hyundai = new Hyundai("Hyundai i20","grey");
+       $Hyundai->Message();
    ?>
+
     
 </body>
 </html>
